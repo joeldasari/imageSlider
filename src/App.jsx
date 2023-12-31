5988a4cbb4fa025c3b7d0d360fd9d82373eb54dc
Joel D <joeldasari10@gmail.com>
2024-01-01 00:33:49 +0530

    changed the btns placement

--- src/App.jsx
+++ src/App.jsx
@@ -46,9 +46,9 @@ const App = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-4 gap-2">
       <p className="text-2xl font-semibold">SUPER HEROS</p>
-      <div style={{backgroundImage: `url(${slides[index].url})`}} className="group w-[720px] h-[480px] max-sm:w-[340px] max-sm:h-[200px] bg-cover bg-center rounded-2xl duration-500">
-      <BsChevronCompactLeft onClick={leftClick} size={30} className=" bg-black/50 text-white rounded-full p-1 hover:cursor-pointer absolute top-[40%] left-[28%] max-sm:top-[15%]  max-sm:left-[12%]" />
-      <BsChevronCompactRight onClick={rightClick} size={30} className=" bg-black/50 text-white rounded-full p-1 hover:cursor-pointer absolute top-[40%] right-[28%] max-sm:top-[15%]  max-sm:right-[12%]"/>
+      <div style={{backgroundImage: `url(${slides[index].url})`}} className="w-[720px] h-[480px] max-sm:w-[340px] max-sm:h-[200px] bg-cover bg-center rounded-2xl duration-500 flex items-center px-6 space-x-[640px] max-sm:px-2 max-sm:space-x-[290px]">
+      <BsChevronCompactLeft onClick={leftClick} size={30} className=" bg-black/50 text-white rounded-full p-1 hover:cursor-pointer absolute" />
+      <BsChevronCompactRight onClick={rightClick} size={30} className=" bg-black/50 text-white rounded-full p-1 hover:cursor-pointer absolute"/>
       </div>
       <div className="flex">
         {slides.map((slideItem,slideIndex) => (
