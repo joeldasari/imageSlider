d489c4dd7ff81acde876bdc9589de2c563066314
Joel D <joeldasari10@gmail.com>
2024-01-01 00:01:08 +0530

    added btns

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import {BsChevronCompactLeft, BsChevronCompactRight} from "react-icons/bs"
+import {RxDotFilled} from "react-icons/rx"
 import { useState } from "react";
 import img1 from "./assets/img1.jpg"
 import img2 from "./assets/img2.jpg"
 import img3 from "./assets/img3.jpg"
 import img4 from "./assets/img4.jpg"
+import img5 from "./assets/img5.jpg"
 const App = () => {
   const slides = [
     {
@@ -17,6 +19,9 @@ const App = () => {
     },
     {
       url: img4,
+    },
+    {
+      url: img5,
     },
   ];
 
@@ -34,13 +39,22 @@ const App = () => {
       setIndex((prev) => prev - 1)
     }
   }
+
+  const changeSlide = (slideIndex) => {
+    setIndex(slideIndex)
+  } 
   return (
     <div className="flex flex-col items-center justify-center mt-4 gap-2">
       <p className="text-2xl font-semibold">SUPER HEROS</p>
       <div style={{backgroundImage: `url(${slides[index].url})`}} className="group w-[720px] h-[480px] max-sm:w-[340px] max-sm:h-[200px] bg-cover bg-center rounded-2xl duration-500">
       <BsChevronCompactLeft onClick={leftClick} size={30} className=" bg-black/50 text-white rounded-full p-1 hover:cursor-pointer absolute top-[40%] left-[28%] max-sm:top-[15%]  max-sm:left-[12%]" />
       <BsChevronCompactRight onClick={rightClick} size={30} className=" bg-black/50 text-white rounded-full p-1 hover:cursor-pointer absolute top-[40%] right-[28%] max-sm:top-[15%]  max-sm:right-[12%]"/>
-      </div>  
+      </div>
+      <div className="flex">
+        {slides.map((slideItem,slideIndex) => (
+          <RxDotFilled key={slideIndex} onClick={() => changeSlide(slideIndex)} className="text-2xl cursor-pointer"/>
+        ))}
+      </div>
     </div>
   );
 };
